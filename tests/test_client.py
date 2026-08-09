7e5a5902abc575d2c32660f7b0e7023bdfef819b
Erik Gafni <erik@typesafe.ai>
2026-08-08 19:37:16 -0700

    Move confidence metric tests

--- tests/test_client.py
+++ tests/test_client.py
@@ -20,10 +20,6 @@ from typesafe_client.api.api_client import (
 from typesafe_client.api.models import ChoiceQuestion, NoulQuestion, ScoreQuestion
 
 from typesafe_client_adapter import TypeSafeClientAdapter
-from typesafe_client_adapter.utils.confidence_metrics import (
-    choice_confidence,
-    score_confidence,
-)
 from typesafe_client_adapter.utils.error_handling import run_with_retries
 
 QUESTIONS = {
@@ -38,23 +34,6 @@ QUESTIONS = {
 
 def test_client_is_typesafe_client():
     assert issubclass(TypeSafeClientAdapter, TypeSafeClient)
-
-
-@pytest.mark.parametrize(
-    ("confidence_metric", "probabilities", "expected_confidence"),
-    [
-        (score_confidence, [0.2] * 5, 0.0),
-        (score_confidence, [0.04] * 5, 0.0),
-        (score_confidence, [0.01, 0.02, 0.07, 0.3, 0.6], 0.55),
-        (choice_confidence, [0.5, 0.5], 0.0),
-        (choice_confidence, [0.2, 0.2], 0.0),
-        (choice_confidence, [0.82, 0.18], 0.64),
-        (score_confidence, [1.0], 1.0),
-        (choice_confidence, [1.0], 1.0),
-    ],
-)
-def test_confidence_metrics(confidence_metric, probabilities, expected_confidence):
-    assert confidence_metric(probabilities) == pytest.approx(expected_confidence)
 
 
 def model_response(response_data, expected_output_mode, expected_descriptions=()):
