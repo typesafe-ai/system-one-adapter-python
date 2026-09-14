271a8ce4e010492d76fcd159a1c04320968b15ce
Erik Gafni <erik@typesafe.ai>
2026-09-14 16:56:35 -0700

    Rename SystemOneClientAdapter to OpenSystemOne

--- tests/utils/test_confidence_metrics.py
+++ tests/utils/test_confidence_metrics.py
@@ -2,7 +2,7 @@
 
 import pytest
 
-from system_one_client_adapter.utils.confidence_metrics import (
+from open_system_one.utils.confidence_metrics import (
     choice_confidence,
     score_confidence,
 )
